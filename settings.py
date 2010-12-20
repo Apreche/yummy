0e36db64507ee9ebe86cfd7a12dc7e578dcd69b6
Issac Kelly <issac.kelly@gmail.com>
2010-12-19 20:59:27 -0500

    pull from templates dir

--- settings.py
+++ settings.py
@@ -86,6 +86,7 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'yummy.urls'
 
 TEMPLATE_DIRS = (
+    os.path.join(os.path.dirname(__file__), 'templates'),
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
