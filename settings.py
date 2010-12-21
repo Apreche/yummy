47ec97fe593154f49bbdb01daf676a4cd333e1c5
Scott Rubin <apreche@frontrowcrew.com>
2010-12-21 10:02:22 -0500

    made local settings template and updated requirements

--- settings.py
+++ settings.py
@@ -88,9 +88,6 @@ ROOT_URLCONF = 'yummy.urls'
 
 TEMPLATE_DIRS = (
     os.path.join(os.path.dirname(__file__), 'templates'),
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
 )
 
 TEMPLATE_CONTEXT_PROCESSORS = (
@@ -110,7 +107,6 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     'django.contrib.admindocs',
     'django.contrib.flatpages',
-    'django_extensions',
     'pagination',
     'taggit',
     'south',
